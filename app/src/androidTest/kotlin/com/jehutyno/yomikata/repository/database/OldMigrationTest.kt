2385ae9510035d071140299a0b767baf2fce1412
Lucas Dooms <lucas.dooms@hotmail.com>
2023-05-19 21:02:20 +0200

    Move androidTest files

--- app/src/androidTest/kotlin/com/jehutyno/yomikata/repository/database/OldMigrationTest.kt
+++ app/src/androidTest/kotlin/com/jehutyno/yomikata/repository/database/OldMigrationTest.kt
@@ -5,7 +5,6 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
-import com.jehutyno.yomikata.repository.database.YomikataDatabase
 import com.jehutyno.yomikata.repository.migration.SqliteTestHelper
 import com.jehutyno.yomikata.repository.migration.Wordv13
 import org.junit.After
