7c8c7803481165edede7d6f19ade54473b30e364
Lucas Dooms <lucas.dooms@hotmail.com>
2023-07-24 12:15:20 +0200

    Add some UI tests

--- app/src/androidTest/kotlin/com/jehutyno/yomikata/screens/quizzes/QuizzesUITest.kt
+++ app/src/androidTest/kotlin/com/jehutyno/yomikata/screens/quizzes/QuizzesUITest.kt
@@ -2,17 +2,17 @@ package com.jehutyno.yomikata.screens.quizzes
 
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.jehutyno.yomikata.R
 import com.jehutyno.yomikata.util.Categories
 import com.jehutyno.yomikata.util.Extras
+import org.hamcrest.Matchers.*
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
@@ -33,13 +33,51 @@ class QuizzesUITest {
 
 
     @Test
-    fun tryout() {
+    fun floatActionButton() {
+        // starts in home screen -> no button
+        onView(withId(R.id.multiple_actions))
+            .check(matches(not(isDisplayed())))
+        // go to a category with button
         activityRule.scenario.onActivity { quizzesActivity ->
             quizzesActivity.gotoCategory(Categories.CATEGORY_HIRAGANA)
         }
+        // check visibility
         onView(withId(R.id.multiple_actions))
-            .perform(click())
             .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun correctTextTitle() {
+        // starts in home screen
+        onView(withId(R.id.text_title))
+            .check(matches(withText(R.string.home_title)))
+        // go to different category
+        activityRule.scenario.onActivity { quizzesActivity ->
+            quizzesActivity.gotoCategory(Categories.CATEGORY_KANJI)
+        }
+        // check if text has changed correctly
+        onView(withId(R.id.text_title))
+            .check(matches(withText(R.string.drawer_kanji_beginner)))
+    }
+
+    @Test
+    fun navViewChangeCategory() {
+        // open nav view
+        onView(allOf(withParent(withId(R.id.toolbar)), isFocusable()))
+            .perform(click())
+        // click on a category
+        onView(withId(R.id.jlpt5_item))
+            .perform(click())
+
+        activityRule.scenario.onActivity { quizzesActivity ->
+            // find resumed, throw error if none found
+            val resumedFragment = quizzesActivity.supportFragmentManager
+                .fragments.first { it.isResumed }
+            val correctFragment = quizzesActivity.supportFragmentManager
+                .findFragmentByTag("f${quizzesActivity.quizzesAdapter.positionFromCategory(Categories.CATEGORY_JLPT_5)}")
+            assert(resumedFragment.equals(correctFragment))
+        }
+
     }
 
 }
